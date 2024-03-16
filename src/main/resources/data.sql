229b4ac52ef6e1b113eed825a53eb46984e2a6d9
saravanan <saravanan_kuppusamy@yahoo.com>
2024-03-16 18:12:09 -0400

    vulab microservice gradle build

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('steve', 'pass', 'steve@accel.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('karthik', 'pass', 'karthi@accel.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('fox', 'pass', 'fox@accel.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('tapas', 'pass', 'tapas@accel.com');
+
+
